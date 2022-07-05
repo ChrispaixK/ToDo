ba3c796ec496cc831c9f3198f408314a68706062
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-07-05 20:28:55 +0200

    eslint error in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+/*disable eslint */
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },

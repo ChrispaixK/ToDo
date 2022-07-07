23833937955a6651acaa9b52fa55c473b9223c23
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-07-07 20:07:48 +0200

    Fix Eslinter errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
-
 import './style.css';
-import './crudop.js'
-const listsEl = document.querySelector('.taskList');
-
+import './crudop.js';
